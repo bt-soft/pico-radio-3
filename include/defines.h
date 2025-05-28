f874b58744aed95e31b7086301f422e3bdbeaa4d
bt-soft <bt-soft@freemail.hu>
2025-05-28 18:34:52 +0200

    Add memory management and utility classes, rotary encoder functionality, and splash screen implementation
    - Introduced PicoMemoryInfo.h for memory status tracking and debugging.
    - Added PicoSensorUtils.h for sensor utility functions including VBUS reading and core temperature.
    - Implemented RotaryEncoder.h and RotaryEncoder.cpp for rotary encoder handling with acceleration and button state management.
    - Created SplashScreen.h and SplashScreen.cpp for displaying a splash screen with program information and progress.
    - Updated README to reflect new features and inspirations.
    - Added rtVars.cpp for runtime variable management including mute and frequency settings.

--- include/defines.h
+++ include/defines.h
@@ -4,11 +4,11 @@
 #include <Arduino.h>
 
 //--- Debug ---
-#define __DEBUG  // Debug mód bekapcsolása
+#define __DEBUG // Debug mód bekapcsolása
 
 #ifdef __DEBUG
-// #define SHOW_MEMORY_INFO
-// #define MEMORY_INFO_INTERVAL 20 * 1000  // 20mp
+#define SHOW_MEMORY_INFO
+#define MEMORY_INFO_INTERVAL 20 * 1000 // 20mp
 
 // Soros portra várakozás a debug üzenetek előtt
 // #define DEBUG_WAIT_FOR_SERIAL
@@ -25,10 +25,12 @@
 // TFT (A TFT_eSPI_User_Setup.h-ban a pinout)
 
 // Feszültségmérés
-#define PIN_VBUS_INPUT A0  // A0/GPIO26 a VBUS bemenethez
+#define PIN_VBUS_INPUT A0      // A0/GPIO26 a VBUS bemenethez
+#define VBUS_DIVIDER_R1 197.5f // Ellenállás VBUS és A0 között (kOhm)
+#define VBUS_DIVIDER_R2 99.5f  // Ellenállás A0 és GND között (kOhm)
 
 // Audio FFT bemenet
-#define AUDIO_INPUT_PIN A1  // A1/GPIO27 az audio bemenethez
+#define AUDIO_INPUT_PIN A1 // A1/GPIO27 az audio bemenethez
 
 // I2C si4735
 #define PIN_SI4735_I2C_SDA 8
@@ -52,8 +54,8 @@
 #define TFT_BACKGROUND_LED_MIN_BRIGHTNESS 5
 
 //--- Battery ---
-#define MIN_BATTERY_VOLTAGE 270  // Minimum akkumulátor feszültség (V*100)
-#define MAX_BATTERY_VOLTAGE 405  // Maximum akkumulátor feszültség (V*100)
+#define MIN_BATTERY_VOLTAGE 270 // Minimum akkumulátor feszültség (V*100)
+#define MAX_BATTERY_VOLTAGE 405 // Maximum akkumulátor feszültség (V*100)
 
 #define TFT_COLOR_DRAINED_BATTERY TFT_COLOR(248, 252, 0)
 #define TFT_COLOR_SUBMERSIBLE_BATTERY TFT_ORANGE
@@ -61,20 +63,17 @@
 //--- ScreenSaver
 #define SCREEN_SAVER_TIMEOUT_MIN 1
 #define SCREEN_SAVER_TIMEOUT_MAX 60
-#define SCREEN_SAVER_TIMEOUT 10  // 10 perc a képernyővédő időzítése
+#define SCREEN_SAVER_TIMEOUT 10 // 10 perc a képernyővédő időzítése
 
 //--- CW Decoder ---
-#define CW_DECODER_DEFAULT_FREQUENCY \
-  750  // Alapértelmezett CW dekóder frekvencia (Hz)
-#define CW_DECODER_MIN_FREQUENCY 600   // Minimum CW dekóder frekvencia (Hz)
-#define CW_DECODER_MAX_FREQUENCY 1500  // Maximum CW dekóder frekvencia (Hz)
+#define CW_DECODER_DEFAULT_FREQUENCY 750 // Alapértelmezett CW dekóder frekvencia (Hz)
+#define CW_DECODER_MIN_FREQUENCY 600     // Minimum CW dekóder frekvencia (Hz)
+#define CW_DECODER_MAX_FREQUENCY 1500    // Maximum CW dekóder frekvencia (Hz)
 
 //--- RTTY mód adatai
-#define RTTY_SPACE_FREQUENCY 2125.0f   // RTTY tér frekvencia (Hz)
-#define RTTY_MARKER_FREQUENCY 2295.0f  // RTTY jelölő frekvencia (Hz)
-#define RTTY_SHIFT_FREQUENCY \
-  RTTY_MARKER_FREQUENCY -    \
-      RTTY_SPACE_FREQUENCY  // RTTY eltolás frekvencia (170Hz)
+#define RTTY_SPACE_FREQUENCY 2125.0f                                      // RTTY tér frekvencia (Hz)
+#define RTTY_MARKER_FREQUENCY 2295.0f                                     // RTTY jelölő frekvencia (Hz)
+#define RTTY_SHIFT_FREQUENCY RTTY_MARKER_FREQUENCY - RTTY_SPACE_FREQUENCY // RTTY eltolás frekvencia (170Hz)
 
 //--- TFT colors ---
 #define TFT_COLOR(r, g, b) (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3))
@@ -94,7 +93,7 @@
 #ifdef __DEBUG
 #define DEBUG(fmt, ...) Serial.printf(fmt __VA_OPT__(, ) __VA_ARGS__)
 #else
-#define DEBUG(fmt, ...)  // Üres makró, ha __DEBUG nincs definiálva
+#define DEBUG(fmt, ...) // Üres makró, ha __DEBUG nincs definiálva
 #endif
 
-#endif  // DEFINES_H
+#endif // DEFINES_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <Arduino.h>
 
+//------------------ TFT
+#include <TFT_eSPI.h>
+TFT_eSPI tft;
+
 #include "Config.h"
+#include "PicoSensorUtils.h"
+#include "rtVars.h"
+
+//------------------- si4735
+#include <SI4735.h>
+SI4735 si4735;
+
+//------------------- Rotary Encoder
+#include <RPi_Pico_TimerInterrupt.h>
+RPI_PICO_Timer rotaryTimer(0); // 0-ás timer használata
+#include "RotaryEncoder.h"
+RotaryEncoder rotaryEncoder = RotaryEncoder(PIN_ENCODER_CLK, PIN_ENCODER_DT, PIN_ENCODER_SW, ROTARY_ENCODER_STEPS_PER_NOTCH);
+#define ROTARY_ENCODER_SERVICE_INTERVAL_IN_MSEC 1 // 1msec
+
 #include "StationStore.h"
 #include "StoreEepromBase.h"
 
@@ -9,18 +27,217 @@ extern Config config;
 extern FmStationStore fmStationStore;
 extern AmStationStore amStationStore;
 
+//------------------- Memória információk megjelenítése
+#ifdef SHOW_MEMORY_INFO
+#include "PicoMemoryInfo.h"
+#endif
+
+//------------------- Képernyők
+#include "SplashScreen.h"
+
+/**
+ * @brief  Hardware timer interrupt service routine a rotaryhoz
+ */
+bool rotaryTimerHardwareInterruptHandler(struct repeating_timer *t) {
+    rotaryEncoder.service();
+    return true;
+}
+
 /**
  * @brief Core0 belépésének inicializálása.
  */
 void setup() {
+#ifdef __DEBUG
     Serial.begin(115200);
-    delay(2000); // Várunk a Serial kapcsolatra
+#endif
+
+    // PICO AD inicializálása
+    PicoSensorUtils::init();
+
+    // Beeper
+    pinMode(PIN_BEEPER, OUTPUT);
+    digitalWrite(PIN_BEEPER, LOW);
+
+    // TFT LED háttérvilágítás kimenet
+    pinMode(PIN_TFT_BACKGROUND_LED, OUTPUT);
+    analogWrite(PIN_TFT_BACKGROUND_LED, TFT_BACKGROUND_LED_MAX_BRIGHTNESS);
+
+    // Rotary Encoder beállítása
+    rotaryEncoder.setDoubleClickEnabled(true);
+    rotaryEncoder.setAccelerationEnabled(true);
+    // Pico HW Timer1 beállítása a rotaryhoz
+    rotaryTimer.attachInterruptInterval(ROTARY_ENCODER_SERVICE_INTERVAL_IN_MSEC * 1000, rotaryTimerHardwareInterruptHandler);
+
+    // TFT inicializálása
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK); // Fekete háttér a splash screen-hez
+
+// Várakozás a soros port megnyitására DEBUG módban
+#ifdef DEBUG_WAIT_FOR_SERIAL
+    Utils::debugWaitForSerial(tft);
+#endif
+
+    // Korai splash screen inicializálás (SI4735 még nincs kész, de a többi infót már meg tudjuk jeleníteni)
+    // Egy üres SI4735 objektummal hozunk létre splash screen-t az általános infókhoz
+    SplashScreen splashEarly(tft, si4735);
+
+    // Csak az általános információkat jelenítjük meg először (SI4735 nélkül)
+    tft.fillScreen(TFT_BLACK);
+
+    // Program cím és build info megjelenítése
+    tft.setFreeFont();
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_CYAN, TFT_BLACK);
+    tft.setTextDatum(TC_DATUM);
+    tft.drawString(PROGRAM_NAME, tft.width() / 2, 20);
+
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextDatum(TC_DATUM);
+    tft.drawString("Version " + String(PROGRAM_VERSION), tft.width() / 2, 50);
+    tft.drawString(PROGRAM_AUTHOR, tft.width() / 2, 70);
+
+    // Build info
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+    tft.setTextDatum(TL_DATUM);
+    tft.drawString("Build: " + String(__DATE__) + " " + String(__TIME__), 10, 100);
+
+    // Inicializálási progress
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.setTextDatum(TC_DATUM);
+    tft.drawString("Initializing...", tft.width() / 2, 140);
+
+    // EEPROM inicializálása (A fordítónak muszáj megadni egy típust, itt most egy Config_t-t használunk, igaziból mindegy)
+    tft.drawString("Loading EEPROM...", tft.width() / 2, 160);
+    StoreEepromBase<Config_t>::init(); // Meghívjuk a statikus init metódust
+
+    // Ha a bekapcsolás alatt nyomva tartjuk a rotary gombját, akkor töröljük a konfigot
+    if (digitalRead(PIN_ENCODER_SW) == LOW) {
+        tft.drawString("Reset detected...", tft.width() / 2, 180);
+        Utils::beepTick();
+        delay(1500);
+        if (digitalRead(PIN_ENCODER_SW) == LOW) { // Ha még mindig nyomják
+            tft.drawString("Loading defaults...", tft.width() / 2, 200);
+            config.loadDefaults();
+            Utils::beepTick();
+            DEBUG("Default settings resored!\n");
+        }
+    } else {
+        // konfig betöltése
+        tft.drawString("Loading config...", tft.width() / 2, 180);
+        config.load();
+    }
+
+    // Kell kalibrálni a TFT Touch-t?
+    if (Utils::isZeroArray(config.data.tftCalibrateData)) {
+        Utils::beepError();
+        Utils::tftTouchCalibrate(tft, config.data.tftCalibrateData);
+    }
+
+    // Beállítjuk a touch scren-t
+    tft.setTouch(config.data.tftCalibrateData); // Állomáslisták betöltése az EEPROM-ból (a config után!) // <-- ÚJ
+    tft.drawString("Loading stations...", tft.width() / 2, 200);
+    fmStationStore.load();
+    amStationStore.load();
+
+    // Az si473x (Nem a default I2C lábakon [4,5] van!!!)
+    Wire.setSDA(PIN_SI4735_I2C_SDA); // I2C for SI4735 SDA
+    Wire.setSCL(PIN_SI4735_I2C_SCL); // I2C for SI4735 SCL
+    Wire.begin();
+
+    // Most átváltunk a teljes splash screen-re az SI4735 infókkal
+    delay(3000); // Kis szünet, hogy lássa a felhasználó az eddigi progress-t
+
+    // Splash screen megjelenítése inicializálás közben
+    SplashScreen splash(tft, si4735);
+
+    // Splash screen megjelenítése progress bar-ral
+    splash.show(true, 6);
+
+    // Lépés 1: I2C inicializálás
+    splash.updateProgress(1, 6, "Initializing I2C...");
+    delay(300);
+
+    // Si4735 inicializálása
+    splash.updateProgress(2, 6, "Detecting SI4735...");
+    int16_t si4735Addr = si4735.getDeviceI2CAddress(PIN_SI4735_RESET);
+    if (si4735Addr == 0) {
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        tft.setTextSize(2);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString("SI4735 NOT DETECTED!", tft.width() / 2, tft.height() / 2);
+        DEBUG("Si4735 not detected");
+        Utils::beepError();
+        while (true) // nem megyünk tovább
+            ;
+    }
+
+    // Lépés 3: SI4735 konfigurálás
+    splash.updateProgress(3, 6, "Configuring SI4735...");
+    si4735.setDeviceI2CAddress(si4735Addr == 0x11 ? 0 : 1); // Sets the I2C Bus Address, erre is szükség van...
+    si4735.setAudioMuteMcuPin(PIN_AUDIO_MUTE);              // Audio Mute pin
+    delay(300);
+
+    DEBUG("Si473X addr: 0x%02X\n", si4735Addr);
+    //--------------------------------------------------------------------
+
+    // Lépés 4: Frekvencia beállítások
+    splash.updateProgress(4, 6, "Setting up frequency...");
+    rtv::freqstep = 1000; // hz
+    rtv::freqDec = config.data.currentBFO;
+    delay(200);
+
+    // Kezdő képernyőtípus beállítása
+    splash.updateProgress(5, 6, "Preparing display...");
+    // Ide jönnen a kezdőképernyő típus beállítása
+    delay(200);
+
+    //--------------------------------------------------------------------
+
+    // Lépés 6: Finalizálás
+    splash.updateProgress(6, 6, "Starting up...");
+    delay(300);
+
+    // Splash screen eltűntetése
+    splash.hide();
+
+    // PICO AD inicializálása
+    PicoSensorUtils::init();
+
+    // Csippantunk egyet
+    Utils::beepTick();
+
+    delay(3000);
 }
 
 /**
  * @brief Core0 fő ciklusa.
  */
-void loop() {}
+void loop() {
+
+//------------------- EEPROM mentés figyelése
+#define EEPROM_SAVE_CHECK_INTERVAL 1000 * 60 * 5 // 5 perc
+    static uint32_t lastEepromSaveCheck = 0;
+    if (millis() - lastEepromSaveCheck >= EEPROM_SAVE_CHECK_INTERVAL) {
+        config.checkSave();
+        fmStationStore.checkSave();
+        amStationStore.checkSave();
+        lastEepromSaveCheck = millis();
+    }
+//------------------- Memória információk megjelenítése
+#ifdef SHOW_MEMORY_INFO
+    static uint32_t lasDebugMemoryInfo = 0;
+    if (millis() - lasDebugMemoryInfo >= MEMORY_INFO_INTERVAL) {
+        debugMemoryInfo();
+        lasDebugMemoryInfo = millis();
+    }
+#endif
+
+    // Rotary Encoder olvasása
+    RotaryEncoder::EncoderState encoderState = rotaryEncoder.read();
+}
 
 /**
  * @brief Core1 belépésének inicializálása.
